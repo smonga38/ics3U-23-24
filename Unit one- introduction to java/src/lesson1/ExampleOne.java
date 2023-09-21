ce5981d5692477352feaf750ea6c07c77d5ae909
Sanjana Monga <smonga@bayviewglen.ca>
2023-09-21 10:27:48 -0400

    ex 2

---
+++ Unit one- introduction to java/src/lesson1/ExampleOne.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class ExampleOne {
+
+    public static void main(String[] args) {
+        System.out.println(4); 
+        System.out.println(35);
+    }
+
+}
---
+++ Unit one- introduction to java/src/lesson1/ExampleTwo.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class ExampleTwo {
+public static void main(String[] args) {
+    System.out.print(3);
+    System.out.println(2);
+    System.out.print(4);
+    System.out.println(3);
+}
+}
